bc1c3960f51c183941c9eb7f7948f295b450d669
Mykhailo Marchuk <nisha.mar4uck@gmail.com>
2023-05-19 18:22:54 +0300

    Initial commit

--- src/main/java/project/library/HelloApplication.java
+++ src/main/java/project/library/HelloApplication.java
@@ -2,22 +2,30 @@ package project.library;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+            Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Library Management");
+            stage.setScene(scene);
+
+            stage.show();
+
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/project/library/HelloController.java
+++ src/main/java/project/library/HelloController.java
@@ -1,6 +1,6 @@
 package project.library;
 
 
-public class HelloController{ {
+public class HelloController{
 
-};
+}
